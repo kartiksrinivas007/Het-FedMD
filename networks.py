97ad2f54c177bd61e41f412793898ab5f6f19936
kartiksrinivas007 <kartiksrinivas008@gmail.com>
2023-09-19 12:24:14 +0530

    Change to new size AlexNet

--- networks.py
+++ networks.py
@@ -139,9 +139,43 @@ class LeNet(nn.Module):
 
 
 
+# ''' AlexNet '''
+# class AlexNet(nn.Module):
+#     def __init__(self, channel, num_classes):
+#         super(AlexNet, self).__init__()
+#         self.features = nn.Sequential(
+#             nn.Conv2d(channel, 128, kernel_size=5, stride=1, padding=4),
+#             nn.ReLU(inplace=True),
+#             nn.MaxPool2d(kernel_size=2, stride=2),
+#             nn.Conv2d(128, 192, kernel_size=5, padding=2),
+#             nn.ReLU(inplace=True),
+#             nn.MaxPool2d(kernel_size=2, stride=2),
+#             nn.Conv2d(192, 256, kernel_size=3, padding=1),
+#             nn.ReLU(inplace=True),
+#             nn.Conv2d(256, 192, kernel_size=3, padding=1),
+#             nn.ReLU(inplace=True),
+#             nn.Conv2d(192, 192, kernel_size=3, padding=1),
+#             nn.ReLU(inplace=True),
+#             nn.MaxPool2d(kernel_size=2, stride=2),
+#         )
+#         self.fc = nn.Linear(192 * 4 * 4, num_classes)
+
+#     def forward(self, x):
+#         x = self.features(x)
+#         x = x.view(x.size(0), -1)
+#         # breakpoint()
+#         print(x.shape)
+#         x = self.fc(x)
+#         return x
+
+#     def embed(self, x):
+#         x = self.features(x)
+#         x = x.view(x.size(0), -1)
+#         return x
+
 ''' AlexNet '''
 class AlexNet(nn.Module):
-    def __init__(self, channel, num_classes):
+    def __init__(self, channel, num_classes, im_size):
         super(AlexNet, self).__init__()
         self.features = nn.Sequential(
             nn.Conv2d(channel, 128, kernel_size=5, stride=1, padding=4),
@@ -158,19 +192,24 @@ class AlexNet(nn.Module):
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
-        self.fc = nn.Linear(192 * 4 * 4, num_classes)
+        if im_size[0] == 32:
+            self.fc = nn.Linear(192 * 4 * 4, num_classes)
+        elif im_size[0] == 96:
+            self.fc = nn.Sequential(
+                    nn.Linear(27648, 192 * 4 * 4),
+                    nn.ReLU(inplace=True),
+                    nn.Linear(192 * 4 * 4, num_classes))
 
     def forward(self, x):
         x = self.features(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
-        return x
+        feature = x.view(x.size(0), -1)
+        out = self.fc(feature)
+        return out
 
     def embed(self, x):
         x = self.features(x)
         x = x.view(x.size(0), -1)
         return x
-
 
 ''' AlexNetBN '''
 class AlexNetBN(nn.Module):
